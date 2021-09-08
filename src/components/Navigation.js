486ac208cdefd46ebf95371b4c2b60f8225a4512
nathan-websculpt <websculpt.nathan@gmail.com>
2021-09-08 13:47:49 -0500

    Adding collapse for Nav on mobile devices

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,15 +2,18 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Navigation = () => (
-<Navbar bg="primary" variant="dark">
+<Navbar collapseOnSelect fixed='top' expamd='sm' bg='primary' variant='dark'>
     <Container>
-    <Navbar.Brand href="#home">React + Solidity Examples</Navbar.Brand>
-    <Nav className="me-auto">
-      <Link to='/' className="nav-link">Home</Link>
-      <Link to='/about' className="nav-link">About</Link>
-      <Link to='/build-notes' className="nav-link">Build Notes</Link>
-      <Link to='/coin-flip' className="nav-link">Coin Flip</Link>
-    </Nav>
+        <Navbar.Toggle aria-controls='responsive-navbar' />
+        <Navbar.Brand href='#home'>React + Solidity Examples</Navbar.Brand>
+        <Navbar.Collapse id='responsive-navbar'>
+            <Nav className='me-auto'>
+                <Link to='/' className='nav-link'>Home</Link>
+                <Link to='/about' className='nav-link'>About</Link>
+                <Link to='/build-notes' className='nav-link'>Build Notes</Link>
+                <Link to='/coin-flip' className='nav-link'>Coin Flip</Link>
+            </Nav>
+        </Navbar.Collapse>
     </Container>
   </Navbar>
 );
