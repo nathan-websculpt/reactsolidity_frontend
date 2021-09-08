b87d6b49baed7ef594b4f0775984c0dd3fff13a1
nathan-websculpt <websculpt.nathan@gmail.com>
2021-09-08 14:00:47 -0500

    bootstrap nav for mobile fixes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,20 @@
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const Navigation = () => (
-<Navbar collapseOnSelect fixed='top' expamd='sm' bg='primary' variant='dark'>
-    <Container>
-        <Navbar.Toggle aria-controls='responsive-navbar' />
+<Navbar collapseOnSelect fixed='top' expand='sm' bg='primary' variant='dark'>
         <Navbar.Brand href='#home'>React + Solidity Examples</Navbar.Brand>
-        <Navbar.Collapse id='responsive-navbar'>
-            <Nav className='me-auto'>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='mr-auto'>
                 <Link to='/' className='nav-link'>Home</Link>
                 <Link to='/about' className='nav-link'>About</Link>
                 <Link to='/build-notes' className='nav-link'>Build Notes</Link>
                 <Link to='/coin-flip' className='nav-link'>Coin Flip</Link>
+                <Link to='/coin-flip' className='nav-link'>Coin Flip</Link>
+                <Link to='/coin-flip' className='nav-link'>Coin Flip</Link>
             </Nav>
         </Navbar.Collapse>
-    </Container>
   </Navbar>
 );
 export default Navigation
